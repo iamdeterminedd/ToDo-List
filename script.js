b58e19561b51a615b6349c3c38eb40839726f4aa
cryptorJim <jimcyril.martinez@gmail.com>
2023-10-03 15:09:06 +0800

    task list and form input styling

--- script.js
+++ script.js
@@ -10,8 +10,25 @@ function onAddTaskSubmit(e) {
   const li = document.createElement('li');
   li.appendChild(document.createTextNode(newTask));
 
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+
   taskList.appendChild(li);
   taskInput.value = '';
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
 }
 
 addTask.addEventListener('submit', onAddTaskSubmit);
